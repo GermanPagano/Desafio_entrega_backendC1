c9c48b6d95f377237376533a18b503c268f8c82c
germanpagano <sergiogermanpagano@gmail.com>
2024-07-07 14:27:42 -0300

    router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = 8080
 //Middlewares
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({extended:true})) // enviar parametros desde url
+app.use(express.json()) //enviar info desde url
+
+
+
+
 app.listen(PORT, () => {console.log(`Server Running on PORT: ${PORT}`)})
