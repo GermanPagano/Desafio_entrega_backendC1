d9ad0d2a0a8380ef6c83191bfefe12213d7bb72d
germanpagano <sergiogermanpagano@gmail.com>
2024-07-07 13:29:46 -0300

    subiendo db

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log(object)
+const express = require('express')
+const app = express()
+const PORT = 8080
+//Middlewares
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.listen(PORT, () => {console.log(`Server Running on PORT: ${PORT}`)})
